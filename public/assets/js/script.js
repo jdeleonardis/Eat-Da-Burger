d0fc6b32b0b5eb4f8e252c2e63c1db2c9ebe9304
jdeleonardis <jdeleonardis@hotmail.com>
2020-06-03 10:17:37 -0400

    pulled js out of index.handlebars into its own js file

---
+++ public/assets/js/script.js
@@ -0,0 +1,42 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  
+  $("#burgeradder").on("submit", function(event) {
+    event.preventDefault(); 
+  
+    var newBurger = {
+      burger: $("#burgername").val().trim()
+    };    
+    
+    // Send the POST request.
+    $.ajax("/api/burger", {
+     type: "POST",
+     data: newBurger
+    }).then(
+      function() {
+       // console.log("created new burger");
+      // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".eatburger").on("click", function(event) {
+    // Get the ID from the button.
+    // This is shorthand for $(this).attr("data-burgerid")
+    var id = $(this).data("burgerid");
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: "eat"
+    }).then(
+      function() {
+        //console.log("updated id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+});
